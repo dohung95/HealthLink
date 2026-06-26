53ddefc04b745a4674d38d58867aaa70e6159f0f
Ndhhiep <hiep.ndh2376@sinhvien.hoasen.edu.vn>
2026-06-26 11:40:26 +0700

    fix: restore home-visit fields in entities, repos & DTOs
    Consultation.java - added homeVisitProposalStatus, homeVisitProposedAt, homeVisitRespondedAt
    Doctor.java - added availableForHomeVisit, homeVisitRadiusKm
    NotificationType.java - added HOME_VISIT_PROPOSED/CONFIRMED/REJECTED
    AppointmentRepository.java - added deleteByStatusAndAppointmentTimeBefore
    ConsultationRepository.java - added homeVisit proposal query method
    DoctorScheduleRepository.java - added findByDoctor_DoctorIdAndAvailableTrue
    AppointmentPayPalCaptureRequest.java - added draftId, scheduleId, bookingDate

--- HealthLink_BE/src/main/java/com/HealthLink/dto/payment/AppointmentPayPalCaptureRequest.java
+++ HealthLink_BE/src/main/java/com/HealthLink/dto/payment/AppointmentPayPalCaptureRequest.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
@@ -50,6 +51,14 @@ public class AppointmentPayPalCaptureRequest {
 
     private Double visitLatitude;
     private Double visitLongitude;
+
+    private Integer draftId;
+
+    private Integer scheduleId;
+
+    private LocalDate bookingDate;
+
+    private Integer sourceConsultationId;
 
     private String paymentMethod = "EWallet";
 
